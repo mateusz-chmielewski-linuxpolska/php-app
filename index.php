a56938d75d151a9a8b17eda07953a5542aac8f31
chmielu1 <maselko@maselko.maselko.local>
2018-02-17 19:54:25 +0100

    ano

--- index.php
+++ index.php
@@ -4,6 +4,25 @@ ostatnie zmiany
 
 ostatnie zmiany + jedna zmiana
 <?php
-$ip = getenv('HTTP_CLIENT_IP')?:
-echo $ip
+#$ip = getenv('HTTP_CLIENT_IP')?:
+#echo $ip;
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+get_client_ip();
 ?>
