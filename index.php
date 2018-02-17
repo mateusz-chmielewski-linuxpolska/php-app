7dee24994584fa4683ef0a0a5e8f18b24b6b7176
chmielu1 <maselko@maselko.maselko.local>
2018-02-17 20:23:06 +0100

    anno

--- index.php
+++ index.php
@@ -7,4 +7,9 @@ ostatnie zmiany + jedna zmiana
 #$ip = getenv('HTTP_CLIENT_IP')?:
 #echo $ip;
 echo $_SERVER;
+echo $_SERVER['SERVER_NAME'];
+echo $_SERVER['REMOTE_ADDR'];
+echo $_SERVER['SERVER_ADDR'];
+echo $_SERVER['SERVER_SOFTWARE'];
+echo $_SERVER['SERVER_NAME'];
 ?>
