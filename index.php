b766dcd91d41adac8bc9a23f20a984b54676c778
chmielu1 <maselko@maselko.maselko.local>
2018-02-17 20:30:12 +0100

    anno

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 Warsztaty OPENSHIFT DAY 2
 
-ostatnie zmiany
+#ostatnie zmiany
 
-ostatnie zmiany + jedna zmiana
+#ostatnie zmiany + jedna zmiana
 <?php
 #$ip = getenv('HTTP_CLIENT_IP')?:
 #echo $ip;
 echo '</br>';
-echo $_SERVER;
-echo $_SERVER['SERVER_NAME'].'</br>';
-echo $_SERVER['REMOTE_ADDR'].'</br>';
+#echo $_SERVER;
+#echo $_SERVER['SERVER_NAME'].'</br>';
+#echo $_SERVER['REMOTE_ADDR'].'</br>';
 echo $_SERVER['SERVER_ADDR'].'</br>';
-echo $_SERVER['SERVER_SOFTWARE'].'</br>';
-echo $_SERVER['SERVER_NAME'].'</br>';
+#echo $_SERVER['SERVER_SOFTWARE'].'</br>';
+#echo $_SERVER['SERVER_NAME'].'</br>';
 ?>
