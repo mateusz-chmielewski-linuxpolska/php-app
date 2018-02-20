44570db3245b28dba0a1e3c1229c6392c52ecdac
chmielu1 <36572312+chmielu1@users.noreply.github.com>
2018-02-20 12:48:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 Warsztaty OPENSHIFT DAY 2
 <br />
+Środowisko produkcyjne, proszę nic nie zmieniać bez zgody.
+<br />
 
-branch 1
-<br />
-nowa zmiana
-<br />
-nowa zmiana 2
-<br />
-nowa zmiana 3
-<br />
-nowa zmiana 4
-<br />
 
 <?php
-#$ip = getenv('HTTP_CLIENT_IP')?:
-#echo $ip;
-echo '</br>';
-#echo $_SERVER;
-#echo $_SERVER['SERVER_NAME'].'</br>';
-#echo $_SERVER['REMOTE_ADDR'].'</br>';
-echo $_SERVER['SERVER_ADDR'].'</br>';
-#echo $_SERVER['SERVER_SOFTWARE'].'</br>';
-#echo $_SERVER['SERVER_NAME'].'</br>';
+echo $_SERVER['SERVER_ADDR'].'<br />';
 ?>
