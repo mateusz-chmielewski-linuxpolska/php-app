55693fad9286f6f3ce244f53b5ca1965aba1aa27
chmielu1 <36572312+chmielu1@users.noreply.github.com>
2018-02-20 14:40:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 Warsztaty OPENSHIFT DAY 2
 <br />
-<<<<<<< HEAD
-Środowisko produkcyjne, proszę nic nie zmieniać bez zgody.
-=======
 Środowisko testowe.
->>>>>>> test
 <br />
 
-
 <?php
-<<<<<<< HEAD
-echo $_SERVER['SERVER_ADDR'].'<br />';
-=======
 echo $_SERVER['SERVER_ADDR'].'<br/>';
->>>>>>> test
 ?>
 Testowanie zmian
