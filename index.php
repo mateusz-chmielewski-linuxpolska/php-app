0c92fd86e3ce24c5a030098fabadd84388149858
chmielu1 <mail@o2.pl>
2018-02-22 10:13:19 +0000

    openshift

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 Warsztaty OPENSHIFT DAY 2
 <br />
-Środowisko testowe.
+Środowisko produkcyjne, proszę nic nie zmieniać
 <br />
-wywolanie reczne
-wywolanie reczne
-do rollbacku
 <?php
 echo $_SERVER['SERVER_ADDR'].'<br />';
 ?>
