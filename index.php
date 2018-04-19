34fbea57b0f35a5b759bc3b3336e06017455039d
chmielu1 <student16@admin1.lab0.example.com>
2018-04-19 11:58:54 +0000

    openshift

--- index.php
+++ index.php
@@ -2,6 +2,9 @@ Warsztaty OPENSHIFT DAY 2
 <br />
 <br />
 Środowisko testowe wersja beta 2.2382389238923
+<br />
+Dobra zmiana, gotowe na produkcje.
+<br />
 Dobra zmiana, gotowe na produkcje.
 <br />
 
