36acecd9905cd4de07ff9e67179b1d56f6a0bec2
chmiel <chmiel@chmielu-local.internal>
2018-04-24 12:26:51 +0200

    test

--- index.php
+++ index.php
@@ -3,12 +3,6 @@ Warsztaty OPENSHIFT DAY 2
 <br />
 Środowisko testowe wersja beta 2.2382389238923
 <br />
-Dobra zmiana, gotowe na produkcje.
-<br />
-Dobra zmiana, gotowe na produkcje.
-<br />
-do TESTa
-<br />
 
 <?php
 echo $_SERVER['SERVER_ADDR'].'<br/>';
