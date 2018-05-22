975e9a90ebbfe65809d3d3061395f593e3e045ca
root <root@docker.openshift.internal>
2018-05-22 14:06:18 +0200

    openshift

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-Warsztaty OPENSHIFT DAY 2
+<h1>OPENSHIFT - source to image</h1>
 <br />
 <br />
-Środowisko testowe.
+Środowisko produkcyjne proszę nic nie zmieniać.
 <br />
+<img src="LinuxPolska-logo.png">
+<img src="lp.png">
 
 <?php
 echo $_SERVER['SERVER_ADDR'].'<br/>';
